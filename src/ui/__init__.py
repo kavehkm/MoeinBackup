5e1e30773c86ae756bd891632d591204004a8e95
kavehkm <mehrbaniankaveh@gmail.com>
2021-02-03 15:46:16 +0330

    create package ui with BaseUI interface in package's root to implement by real ui

---
+++ src/ui/__init__.py
@@ -0,0 +1,44 @@
+class BaseUI(object):
+    """Base UI"""
+    def __init__(self):
+        self._bootstrap()
+
+    def _bootstrap(self):
+        self._create_ui()
+        self._connect_signals()
+
+    def _create_ui(self):
+        pass
+
+    def _connect_signals(self):
+        pass
+
+    def get_settings(self):
+        pass
+
+    def set_settings(self):
+        pass
+
+    def show_message(self, msg, code):
+        bg_color = 'white'
+        if code == 0:
+            # error
+            bg_color = 'red'
+        elif code == 1:
+            # success
+            bg_color = 'green'
+        print(bg_color, ': ', msg)
+
+    def running(self):
+        print('running...')
+
+    def stopped(self):
+        print('stopped...')
+
+    def connecting(self):
+        print('connecting...')
+
+
+class UI(BaseUI):
+    """UI"""
+    pass
