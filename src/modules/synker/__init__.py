d2cab0c6355110bdb4e341c5bfff77a64f371f1c
kavehkm <mehrbaniankaveh@gmail.com>
2021-02-15 10:40:40 +0330

    create module synker: Synker class and providers module

---
+++ src/modules/synker/__init__.py
@@ -0,0 +1,152 @@
+# standard
+import os
+import bisect
+import pathlib
+# internal
+from src.errors import ModuleError
+from src.modules import BaseModule
+from src.modules.synker.providers import ProviderAPIError, DropboxProvider
+
+
+class File(object):
+    """File"""
+    def __init__(self, path, size, mtime):
+        self._path = path
+        self._size = size
+        self._mtime = mtime
+
+    @property
+    def path(self):
+        return self._path
+
+    @property
+    def size(self):
+        return self._size
+
+    @property
+    def mtime(self):
+        return self._mtime
+
+    def __eq__(self, other):
+        return self.mtime == other.mtime
+
+    def __ne__(self, other):
+        return self.mtime != other.mtime
+
+    def __gt__(self, other):
+        return self.mtime > other.mtime
+
+    def __ge__(self, other):
+        return self.mtime >= other.mtime
+
+    def __lt__(self, other):
+        return self.mtime < other.mtime
+
+    def __le__(self, other):
+        return self.mtime <= other.mtime
+
+
+class FileQueue(object):
+    """File Queue"""
+    def __init__(self):
+        self._files = []
+        self._used_space = 0
+
+    @property
+    def used_space(self):
+        return self._used_space
+
+    def push(self, file):
+        bisect.insort(self._files, file)
+        self._used_space += file.size
+
+    def pop(self):
+        file = self._files.pop(0)
+        self._used_space -= file.size
+        return file
+
+    def head(self):
+        return self._files[-1]
+
+    def tail(self):
+        return self._files[0]
+
+
+class Synker(BaseModule):
+    """Synker"""
+    def __init__(self, interval, localdir, pattern, clouddir, limit, token):
+        super().__init__(interval)
+        # validation
+        error = ''
+        if not localdir or not os.access(localdir, 7):
+            error = 'invalid localdir'
+        elif not pattern:
+            error = 'pattern is required'
+        elif not clouddir or not clouddir.startswith('/'):
+            error = 'invalid clouddir'
+        elif not token:
+            error = 'token is required'
+        else:
+            try:
+                limit = int(limit)
+            except Exception:
+                error = 'invalid limit'
+        if error:
+            raise ModuleError(error)
+        self._localdir = localdir
+        self._pattern = pattern
+        self._clouddir = clouddir
+        self._limit = limit
+        self._provider = DropboxProvider(token)
+        self._cloud = None
+
+    @property
+    def cloud(self):
+        if self._cloud is None:
+            fq = FileQueue()
+            try:
+                for f in self._provider.get_files(self._clouddir):
+                    fq.push(File(*f))
+            except ProviderAPIError:
+                pass
+            self._cloud = fq
+        return self._cloud
+
+    @property
+    def max_mtime(self):
+        try:
+            head = self.cloud.head()
+        except IndexError:
+            mtime = 0
+        else:
+            mtime = head.mtime
+        return mtime
+
+    @property
+    def remain_space(self):
+        return self._limit - self.cloud.used_space
+
+    @property
+    def new_files(self):
+        mtime = self.max_mtime
+        return sorted([
+            entry for entry in pathlib.Path(self._localdir).rglob(self._pattern)
+            if entry.is_file() and entry.stat().st_mtime > mtime and entry.stat().st_size < self._limit
+        ], key=lambda f: f.stat().st_mtime)
+
+    def _do(self):
+        for nf in self.new_files:
+            while nf.stat().st_size > self.remain_space:
+                tail = self.cloud.tail()
+                try:
+                    self._provider.delete(tail.path)
+                except ProviderAPIError:
+                    pass
+                self.cloud.pop()
+            content = nf.read_bytes()
+            size = nf.stat().st_size
+            mtime = nf.stat().st_mtime
+            relpath = nf.relative_to(self._localdir).as_posix()
+            path = self._clouddir + '/' + relpath
+            self._provider.upload(content, path, mtime)
+            self.cloud.push(File(path, size, mtime))
---
+++ src/modules/synker/providers.py
@@ -0,0 +1,148 @@
+# standard
+import json
+import requests
+from datetime import datetime
+# internal
+from src.errors import NetworkError, ModuleError
+
+
+class ProviderError(ModuleError):
+    """Provider Error"""
+    msg = 'cloud provider base error'
+
+
+class ProviderBadInputError(ProviderError):
+    """Provider Bad Input Error"""
+    msg = 'cloud provider bad input parameter'
+
+
+class ProviderAuthError(ProviderError):
+    """Provider Auth Error"""
+    msg = 'cloud provider invalid authentication credentials'
+
+
+class ProviderAccessError(ProviderError):
+    """Provider Access Error"""
+    msg = 'cloud provider access dined'
+
+
+class ProviderAPIError(ProviderError):
+    """Provider API Error"""
+    msg = 'cloud provider api error'
+
+
+class ProviderLimitReachError(ProviderError):
+    """Provider Limit Reach Error"""
+    msg = 'cloud provider limit reach'
+
+
+class ProviderInternalError(ProviderError):
+    """Provider Internal Error"""
+    msg = 'cloud provider internal error'
+
+
+class Provider(object):
+    """Base Provider"""
+    def get_files(self, path, recursive=True):
+        pass
+
+    def upload(self, content, path, mtime):
+        pass
+
+    def download(self, path):
+        pass
+
+    def delete(self, path):
+        pass
+
+
+class DropboxProvider(Provider):
+    """Dropbox Provider"""
+    _datetime_format = '%Y-%m-%dT%H:%M:%SZ'
+    _urls = {
+        'account_info': 'https://api.dropboxapi.com/2/users/get_current_account',
+        'get_files':    'https://api.dropboxapi.com/2/files/list_folder',
+        'upload':       'https://content.dropboxapi.com/2/files/upload',
+        'download':     'https://content.dropboxapi.com/2/files/download',
+        'delete':       'https://api.dropboxapi.com/2/files/delete'
+    }
+
+    def __init__(self, token):
+        self._token = token
+        self._check_token()
+
+    def _check_token(self):
+        self._send_request(self._urls['account_info'])
+
+    def _mtime2iso(self, mtime):
+        return datetime.fromtimestamp(mtime).strftime(self._datetime_format)
+
+    def _iso2mtime(self, iso):
+        return datetime.strptime(iso, self._datetime_format).timestamp()
+
+    def _send_request(self, url, **p):
+        headers = p.get('headers', {})
+        headers['Authorization'] = 'Bearer {}'.format(self._token)
+        p['headers'] = headers
+        try:
+            r = requests.post(url, **p)
+        except requests.exceptions.RequestException:
+            raise NetworkError
+        else:
+            if r.status_code == 200:
+                return r
+            elif r.status_code == 400:
+                raise ProviderBadInputError(details=r.text)
+            elif r.status_code == 401:
+                raise ProviderAuthError(details=r.json()['error_summary'])
+            elif r.status_code == 403:
+                raise ProviderAccessError
+            elif r.status_code == 409:
+                raise ProviderAPIError(details=r.json()['error_summary'])
+            elif r.status_code == 429:
+                raise ProviderLimitReachError
+            elif r.status_code == 500:
+                raise ProviderInternalError
+
+    def get_files(self, path, recursive=True):
+        p = {
+            'headers': {
+                'Content-Type': 'application/json'
+            },
+            'json': {
+                'path': path,
+                'recursive': recursive
+            }
+        }
+        r = self._send_request(self._urls['get_files'], **p).json()
+        return [
+            [entry['path_display'], entry['size'], self._iso2mtime(entry['client_modified'])]
+            for entry in r['entries'] if entry['.tag'] == 'file'
+        ]
+
+    def upload(self, content, path, mtime):
+        p = {
+            'headers': {
+                'Dropbox-API-Arg': json.dumps({
+                    'path': path,
+                    'mode': 'overwrite',
+                    'client_modified': self._mtime2iso(mtime)
+                }),
+                'Content-Type': 'application/octet-stream'
+            },
+            'data': content
+        }
+        self._send_request(self._urls['upload'], **p)
+        print('{} * {} uploaded to cloud successfully.'.format(path, mtime))
+
+    def delete(self, path):
+        p = {
+            'headers': {
+                'Content-Type': 'application/json'
+            },
+            'json': {
+                'path': path
+            }
+        }
+        self._send_request(self._urls['delete'], **p)
+        print('{} deleted from cloud.'.format(path))
